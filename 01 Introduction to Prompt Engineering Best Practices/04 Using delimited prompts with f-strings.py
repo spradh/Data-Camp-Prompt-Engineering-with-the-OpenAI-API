3332d7ed14b6291008f6d377e728c16234fec101
Sabin <spradh@users.noreply.github.com>
2026-05-27 14:57:49 -0500

    Create 04 Using delimited prompts with f-strings.py

---
+++ 01 Introduction to Prompt Engineering Best Practices/04 Using delimited prompts with f-strings.py
@@ -0,0 +1,11 @@
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+# Create a prompt that completes the story
+prompt = f"""Complete the story delimited by triple backticks. 
+ ```{story}```"""
+
+# Get the generated response 
+response = get_response(prompt)
+
+print("\n Original story: \n", story)
+print("\n Generated story: \n", response)
