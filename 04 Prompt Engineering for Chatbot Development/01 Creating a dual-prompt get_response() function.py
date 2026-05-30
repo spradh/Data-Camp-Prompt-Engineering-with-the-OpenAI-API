bbe7eda5e6912ca73785029e8dacd92485bdfd7b
Sabin <spradh@users.noreply.github.com>
2026-05-29 22:56:38 -0500

    Create 01 Creating a dual-prompt get_response() function.py

---
+++ 04 Prompt Engineering for Chatbot Development/01 Creating a dual-prompt get_response() function.py
@@ -0,0 +1,14 @@
+client = OpenAI(api_key="<OPENAI_API_TOKEN>")
+
+def get_response(system_prompt, user_prompt):
+  # Assign the role and content for each message
+  messages = [{"role": "system", "content": system_prompt},
+      		  {"role": "user", "content": user_prompt}]  
+  response = client.chat.completions.create(
+      model="gpt-4o-mini", messages= messages, temperature=0)
+  
+  return response.choices[0].message.content
+
+# Try the function with a system and user prompts of your choice 
+response = get_response("system_prompt", "user_prompt")
+print(response)
